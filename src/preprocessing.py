c6c3a23a98c1f27e11f6f32a43cff4851b6b84bb
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2026-02-04 19:46:31 +0100

    preprocess done

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
 def open_file(file_path):
@@ -14,24 +12,18 @@ def open_file(file_path):
     return None
 
 
-def fill_desc_nulls(data):
+def basic_cleaning(data):
+    # Remove duplicates
+    data = data.drop_duplicates()
+    data = data.dropna(subset=['CustomerID'])
+    data.reset_index(drop=True, inplace=True)
+
     # Create a dictionary of StockCode and Description from rows where Description is not null
     valid_desc_map = data.dropna(subset=['Description']).drop_duplicates('StockCode').set_index('StockCode')['Description']
 
     # Fill missing descriptions by mapping the StockCode to the valid descriptions
     data['Description'] = data['Description'].fillna(data['StockCode'].map(valid_desc_map))
     data['Description'] = data['Description'].fillna('No Description')
-
-
-def fill_customer_nulls(data):
-    # Fill missing CustomerID values with 0
-    data['CustomerID'] = data['CustomerID'].fillna(0)
-
-
-def basic_cleaning(data):
-    # Remove duplicates
-    data = data.drop_duplicates()
-    data.reset_index(drop=True, inplace=True)
 
     # Remove rows with InvoiceNo starting with 'C' (cancellations)
     data = data[~data['InvoiceNo'].str.startswith('C', na=False)]
@@ -42,19 +34,18 @@ def basic_cleaning(data):
     # Remove rows with non-positive Quantity or UnitPrice
     data = data[data['Quantity'] > 0]
     data = data[data['UnitPrice'] > 0]
+
+    # Create TotalPrice column
+    data['TotalPrice'] = data['Quantity'] * data['UnitPrice']
     return data
 
 
-def main():
-    data_path = "..\\data\\online_retail.csv"
-    data = open_file(data_path)
-    if data is None:
-        print("Data loading failed. Exiting.")
-        return
-    fill_desc_nulls(data)
-    fill_customer_nulls(data)
-    data = basic_cleaning(data)
+def build_rfm(data):
+    rfm = (data.groupby('CustomerID').agg({
+        'InvoiceDate': lambda x: (data['InvoiceDate'].max() - x.max()).days,
+        'InvoiceNo': 'nunique',
+        'TotalPrice': 'sum'
+    }).reset_index())
 
-
-if __name__ == "__main__":
-    main()
+    rfm.columns = ['CustomerID', 'Recency', 'Frequency', 'Monetary']
+    return rfm
